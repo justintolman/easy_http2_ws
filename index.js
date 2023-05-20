e21065cfab27247386b2ad51821338ccd120096f
Justin Tolman <58009905+justintolman@users.noreply.github.com>
2023-05-20 12:31:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import http from 'http';
 
-export class HEllo {
+export class Hello {
+	constructor(){
+		this._target = 'Node World';
+	}
 	start(){
 		http.createServer(function (req, res) {
 			res.writeHead(200, {'Content-Type': 'text/html'});
-			res.end('Hello World!');
+			res.end(`Hello ${this._target}!`);
 			}).listen(80);
 	}
 }
