d7cabbeaeba4d10492f266b83f5dd36c2e135a2e
Justin Tolman <58009905+justintolman@users.noreply.github.com>
2023-05-21 03:38:28 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 import {WebServer} from './src/http2.js';
-import {SocketServer} from './src/socket.js';
+// import {SocketServer} from './src/socket.js';
 
-export {WebServer, SocketServer};
+export {WebServer};
+// export {WebServer, SocketServer};
