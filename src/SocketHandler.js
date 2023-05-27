ff56d7e5c350a3ad1da6ded048647cc8f4afb505
Justin Tolman <58009905+justintolman@users.noreply.github.com>
2023-05-27 14:09:50 -0600

    Update SocketHandler.js

--- src/SocketHandler.js
+++ src/SocketHandler.js
@@ -81,7 +81,7 @@ class Room {
 		 */
 		if(msg.direct){
 			let recipient = this._clients.find(client=>client.id === direct);
-			if(recipient) recipient.hear({msg));
+			if(recipient) recipient.hear({msg});
 		} else {
 			this._clients.map(client => if(client.id !== msg.sender.id) client.hear({msg));
 			this._messages.push(msg);
