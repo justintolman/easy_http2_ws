7da28abcbd9a1cf0da3deca3557e2f02af16e308
Justin Tolman <58009905+justintolman@users.noreply.github.com>
2023-07-01 17:15:13 -0600

    Update BuildFiles.js

--- src/BuildFiles.js
+++ src/BuildFiles.js
@@ -267,6 +267,7 @@ export class BuildFiles {
 			for await (let file of files){
 				// Filter out hidden files
 				if(file.startsWith('.')) continue;
+				if(r_data.hidden) continue;
 				if(cfg.hidden_files && file.match(new RegExp(`(${cfg.hidden_files.join('|')})$`))) continue;
 				let f_path = path.join(pth, file);
 				let stats = await fs.stat(f_path);
