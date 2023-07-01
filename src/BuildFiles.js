0e1ad939210886e67c15bbcfe21260da2df6d54d
Justin Tolman <58009905+justintolman@users.noreply.github.com>
2023-07-01 15:13:54 -0600

    Update BuildFiles.js

--- src/BuildFiles.js
+++ src/BuildFiles.js
@@ -106,6 +106,7 @@ export class BuildFiles {
 			default:
 				this.robots += 'Allow: ' + route +'\n';
 		}
+		 console.log(r_data)
 		if(r_data.hidden) return;
 		if(r_data.hidden) console.log('Hidden: ' + route)
 		if(!(this._templates || cfg.sitemap)) return;
